9990d616c246371bcae387c282e2583b63a957fd
William Coletto <william.coletto@prolucid.ca>
2017-01-25 20:56:09 -0500

    adding sleep to startup script

--- start-notebook.sh
+++ start-notebook.sh
@@ -3,4 +3,5 @@ docker stop jupyter
 docker rm jupyter
 docker run -d -v `pwd`:/notebooks -p 8888:8888 --name jupyter ifsharp:3.0.0-alpha 
 echo "copy and paste the below link to get into the web interface"
+sleep 3
 docker exec -it jupyter jupyter notebook list
